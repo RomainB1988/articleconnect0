31d8b2c5638fef83132fae6326b35bb4c5b9a97f
Romain Buisine rom.buisine@gmail.com <rom.buisine@gmail.com>
2023-07-20 09:00:14 +0200

    publication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts
+  has_many :comments
+
   validates :nickname, presence: true, length: { maximum: 50 }
   validates :nickname, presence: { message: "Veuillez saisir un pseudonyme" }
 end
